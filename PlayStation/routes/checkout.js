6f1b06c5e5af4922147b15d849de9ab8f72f3584
stefaniazocco <stefaniazocco93@gmail.com>
2022-12-10 00:44:21 +0100

    fix: checkout route

--- PlayStation/routes/checkout.js
+++ PlayStation/routes/checkout.js
@@ -2,26 +2,32 @@ const router = require("express").Router();
 const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_KEY)
 
-const MY_DOMAIN = 'http://localhost:3000/cart';
+const MY_DOMAIN = 'http://localhost:3000/';
 
 
 router.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: "eur",
-            product_data: {
-              name: "hoodie",
-            },
-            unit_amount: 7000,
-          },
-          quantity: 1,
+  const line_items = req.body.product.map((item) => {
+    return {
+      price_data: {
+        currency: "eur",
+        product_data: {
+          name: item.name,
+          // images: [item.image],    //non funziona ma funzionerà
+          description: req.body.size.find(s => s.name === item.name)?.size,
+          metadata: {
+            id: item.number
+          }
         },
-      ],
+        unit_amount: item.price * 100,
+      },
+      quantity: req.body.counter.find(c => c.name === item.name).quantity,
+    }
+  })  
+  const session = await stripe.checkout.sessions.create({
+      line_items,
       mode: 'payment',
-      success_url: `${MY_DOMAIN}?success=true`,
-      cancel_url: `${MY_DOMAIN}?canceled=true`,
+      success_url: `${MY_DOMAIN}`,
+      cancel_url: `${MY_DOMAIN}cart`,
     });
   
     res.send({url: session.url});
